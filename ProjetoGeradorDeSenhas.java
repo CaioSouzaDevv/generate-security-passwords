fb116caf504608077cb2c9a0eedada32e0c4dd4c
Caio de Souza Silva <caio.silva@lojastorra.com.br>
2026-03-24 16:03:10 -0300

    feat: initial-commit

---
+++ ProjetoGeradorDeSenhas.java
@@ -0,0 +1,6 @@
+import java.security.SecureRandom;
+
+public class ProjetoGeradorDeSenhas {
+    private static final String CARACTERES = "qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM123456789()@#$%¨!.,;/~[´";
+
+}
